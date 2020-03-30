9a735185d7a035664b719cc996e6a02c331f0b96
4coma <44037654+4coma@users.noreply.github.com>
2020-03-30 15:30:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const mType = [500, 200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05];
+const mNb = [];
+var newVal = 0;
 
+var input = 23501;
+newVal = input;
+
+
+for (var i = 0; i < mType.length; i++){
+  mNb.push(Math.trunc(newVal / mType[i]));
+  newVal = newVal - (mNb[i] * mType[i]);
+};
+
+console.log(mNb);
